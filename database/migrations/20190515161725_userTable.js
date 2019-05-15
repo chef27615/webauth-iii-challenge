4505636023684a31938482f881fb3f26a3a7679b
wei du <weidu@weis-Mac-mini.hsd1.va.comcast.net>
2019-05-15 16:21:59 -0400

    initiated the project, dependencies linked, base server functions are up and running

---
+++ database/migrations/20190515161725_userTable.js
@@ -0,0 +1,24 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', tbl => {
+      tbl.increments();
+      
+      tbl
+      .string('name', 128)
+      .notNullable()
+      .unique()
+
+      tbl
+      .string('password', 256)
+      .notNullable()
+
+      tbl
+      .string('department', 128)
+      .notNullable()
+
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('users');
+};
